a8dc59c36ec21e7fde5b84fea41eb1b281817ef6
mixiHatter <5281cmixi@gmail.com>
2023-07-06 11:14:00 +0300

    complited~!

--- secretMessage/Program.cs
+++ secretMessage/Program.cs
@@ -10,6 +10,27 @@ namespace secretMessage
     {
         static void Main(string[] args)
         {
+            string password = "Valera";
+            string secretMessage = "Ты самый лучший! У тебя всё получиться!";
+            string userInput;
+            int numberOfTries = 3;
+
+            for (numberOfTries = 3; numberOfTries > 0; numberOfTries--)
+            {
+                Console.WriteLine("Введите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine(secretMessage);
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверно!");
+                }
+            }
         }
     }
 }
