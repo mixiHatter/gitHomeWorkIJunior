6d801ae6f05d15b1a5761cde426cbeabda8e4389
mixiHatter <5281cmixi@gmail.com>
2023-07-06 12:51:50 +0300

    complited withoutOperators

--- withoutOperators/Program.cs
+++ withoutOperators/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace withoutOperators
@@ -10,9 +11,29 @@ namespace withoutOperators
     {
         static void Main(string[] args)
         {
+            int numberMin = 1;
+            int numberMax = 28;
+            int minNumberCheck = 100;
+            int maxNumberCheck = 999;
+            int count = 0;
+            Random random = new Random();
+            int N = random.Next(numberMin, numberMax);
+
+            Console.WriteLine($"Число N = {N}");
+
+            for(int i = minNumberCheck; i <= maxNumberCheck; i += 1)
+            {
+                for (int j = N; j <= maxNumberCheck; j += N)
+                {
+                    if (j == i)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Количество трёхзначных натуральных чисел кратны N = {count}");
+            Console.ReadKey();
         }
     }
 }
-
-/* Дано N (1 ≤ N ≤ 27). Найти количество трехзначных натуральных чисел, которые кратны N. Операции деления (/, %) не использовать. А умножение не требуется.
-Число N всего одно, его надо получить в нужном диапазоне. */
